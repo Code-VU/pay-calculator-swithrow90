1883eca9ce6113f6ca9e2e590a08765e12500dc3
Shane Withrow <swithrow90@gmail.com>
2022-01-19 22:52:38 -0600

    created gross pay program

--- payCalculator.py
+++ payCalculator.py
@@ -1,8 +1,14 @@
 def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
+    
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = input("Enter Hours: ")
+    rate = input("Enter Rate: ")
+
+    PAY = float(hrs) * float(rate)
+
+    print(PAY)
     
     # end assignment
 
@@ -10,4 +16,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+#calculatePay()
